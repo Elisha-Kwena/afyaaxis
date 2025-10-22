607541143d06f100ca9c1a056946625fd640fc96
harrison opondo otieno <159556298+Meatycrude@users.noreply.github.com>
2025-10-22 12:48:25 +0300

    Rename and update patient records page
    Renamed the records page to health-records and updated the component to use a dynamic title and improved layout. The Navbar now receives a title prop, and the main content area has been refactored for better styling and responsiveness.

--- client/src/app/(patient)/health-records/page.tsx
+++ client/src/app/(patient)/health-records/page.tsx
@@ -4,18 +4,21 @@ import Navbar from "@/components/navigation/Navbar";
 import { useSidebar } from "@/contexts/SidebarContext";
 
 export default function Records() {
+  const title = "Health-Records";
   const { isOpen } = useSidebar();
   return (
     <>
-      <Navbar />
+      <Navbar title={title} />
       <section
         className={`p-1 transition-all min-h-screen  bg-white-200 duration-300 pt-[59px] ${
           isOpen ? "md:ml-[300px]" : "md:ml-[55px]"
         }`}
       >
-        {" "}
-        <div>Records Page</div>
-        <div>Records Page</div>
+        <div
+          className={`h-screen pb-[60px] overflow-scroll px-1 fixed right-0 transition-all duration-700 ease-in-out ${
+            isOpen ? "left-[305px]" : "left-[60px]"
+          }`}
+        ></div>
       </section>
     </>
   );
